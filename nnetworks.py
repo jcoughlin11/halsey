1cc708430c154d450c0eee92c6a4dd5651e26a41
Jared Coughlin <jcoughl2@nd.edu>
2019-04-11 15:49:28 -0500

    Added the build_dueling1_net() function to the DQN class

--- nnetworks.py
+++ nnetworks.py
@@ -6,7 +6,10 @@ Purpose: Contains the class definitions of neural networks
 Notes:
 """
 import tensorflow as tf
+from tensorflow.contrib.layers import xavier_initializer
+from tensorflow.contrib.layers import xavier_initializer_conv2d
 from tensorflow.train import AdamOptimizer
+from tensorflow.train import RMSPropOptimizer
 
 
 
@@ -46,6 +49,8 @@ class DQN():
         # Build the network
         if self.arch == 'conv1':
             self.build_conv1_net()
+        elif self.arch == 'dueling1':
+            self.build_dueling1_net()
         else:
             raise ValueError("Error, unrecognized network architecture!")
 
@@ -54,7 +59,8 @@ class DQN():
     #-----
     def build_conv1_net(self):
         """
-        This function constructs the layers of the network.
+        This function constructs the layers of the network. Uses three convolutional
+        layers followed by a FC and then output layer
         """
         with tf.variable_scope(self.name):
             # Placeholders (anything that needs to be fed from the outside)
@@ -71,60 +77,141 @@ class DQN():
             self.target_Q = tf.placeholder(tf.float32,
                                            shape=[None],
                                            name='target')
-
             # First convolutional layer
             self.conv1 = tf.layers.conv2d(inputs=self.inputs,
                 filters=32,
                 kernel_size=[8,8],
                 strides=[4,4],
                 padding='VALID',
-                kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                kernel_initializer=xavier_initializer_conv2d(),
                 name='conv1')
             # First convolutional layer activation
             self.conv1_out = tf.nn.elu(self.conv1, name='conv1_out')
-
             # Second convolutional layer
             self.conv2 = tf.layers.conv2d(inputs=self.conv1_out,
                 filters=64,
                 kernel_size=[4,4],
                 strides=[2,2],
                 padding='VALID',
-                kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                kernel_initializer=xavier_initializer_conv2d(),
                 name='conv2')
             # Second convolutional layer activation
             self.conv2_out = tf.nn.elu(self.conv2, name='conv2_out')
-
             # Third convolutional layer
             self.conv3 = tf.layers.conv2d(inputs=self.conv2_out,
                 filters=64,
                 kernel_size=[3,3],
                 strides=[2,2],
                 padding='VALID',
-                kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                kernel_initializer=tf.xavier_initializer_conv2d(),
                 name='conv3')
             # Third convolutional layer activation
             self.conv3_out = tf.nn.elu(self.conv3, name='conv3_out')
-
             # Flatten
             self.flatten = tf.contrib.layers.flatten(self.conv3_out)
             # FC layer
             self.fc = tf.layers.dense(inputs=self.flatten,
                 units=512,
                 activation=tf.nn.elu,
-                kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                kernel_initializer=tf.xavier_initializer(),
                 name='fc1')
             # Output layer (FC)
             self.output = tf.layers.dense(inputs=self.fc,
                 units=self.nActions,
                 activation=None,
-                kernel_initializer=tf.contrib.layers.xavier_initializer())
-
+                kernel_initializer=xavier_initializer())
             # Get the predicted Q value. 
             self.Q = tf.reduce_sum(tf.multiply(self.output, self.actions))
-
             # Get the error (MSE)
             self.loss = tf.reduce_mean(tf.square(self.target_Q - self.Q))
-
             # Optimizer
             self.optimizer = AdamOptimizer(self.learningRate).minimize(self.loss)
+        self.saver = tf.train.Saver()
+
+    #-----
+    # build_dueling1_net
+    #-----
+    def build_dueling_net(self):
+        """
+        This function constructs the layers of the network. Three conv layers followed by
+        dueling DQN, which splits into two streams: one for value and one for advantage
+        before recominging them with an aggregation layer
+        """
+        with tf.variable_scope(self.name):
+            # Placeholders (anything that needs to be fed from the outside)
+            # Input. This is the stack of frames from the game
+            self.inputs = tf.placeholder(tf.float32,
+                                        shape=self.inputShape,
+                                        name='inputs')
+            # Actions. The action the agent chose. Used to get the predicted Q value
+            self.actions = tf.placeholder(tf.float32,
+                                          shape=[None, 1],
+                                          name='actions')
+            # Target Q. The max discounted future reward playing from next state
+            # after taking chosen action. Determined by Bellmann equation.
+            self.target_Q = tf.placeholder(tf.float32,
+                                           shape=[None],
+                                           name='target')
+            # First convolutional layer
+            self.conv1 = tf.layers.conv2d(inputs=self.inputs,
+                filters=32,
+                kernel_size=[8,8],
+                strides=[4,4],
+                padding='VALID',
+                kernel_initializer=xavier_initializer_conv2d(),
+                name='conv1')
+            # First convolutional layer activation
+            self.conv1_out = tf.nn.elu(self.conv1, name='conv1_out')
+            # Second convolutional layer
+            self.conv2 = tf.layers.conv2d(inputs=self.conv1_out,
+                filters=64,
+                kernel_size=[4,4],
+                strides=[2,2],
+                padding='VALID',
+                kernel_initializer=xavier_initializer_conv2d(),
+                name='conv2')
+            # Second convolutional layer activation
+            self.conv2_out = tf.nn.elu(self.conv2, name='conv2_out')
+            # Third convolutional layer
+            self.conv3 = tf.layers.conv2d(inputs=self.conv2_out,
+                filters=64,
+                kernel_size=[3,3],
+                strides=[2,2],
+                padding='VALID',
+                kernel_initializer=xavier_initializer_conv2d(),
+                name='conv3')
+            # Third convolutional layer activation
+            self.conv3_out = tf.nn.elu(self.conv3, name='conv3_out')
+            # Flatten
+            self.flatten = tf.contrib.layers.flatten(self.conv3_out)
+            # Value stream
+            self.value_fc = tf.layers.dense(inputs=self.flatten,
+                                            units=512,
+                                            activation=tf.nn.elu,
+                                            kernel_initializer=xavier_initializer(),
+                                            name='value_fc')
+            self.value = tf.layers.dense(inputs=self.value_fc,
+                                        units=1,
+                                        activation=None,
+                                        kernel_initializer=xavier_initializer(),
+                                        name='value')
+            # Advantage stream
+            self.advantage_fc = tf.layers.dense(inputs=self.flatten,
+                                                units=512,
+                                                activation=tf.nn.elu,
+                                                kernel_initializer=xavier_initializer(),
+                                                name='advantage_fc')
+            self.advantage = tf.layers.dense(inputs=self.advantage_fc,
+                                            units=self.nActions,
+                                            activation=None,
+                                            kernel_initializer=xavier_initializer(),
+                                            name='advantage')
+            # Aggregation layer: Q(s,a) = V(s) + [A(s,a) - 1/|A| * sum_{a'} A(s,a')]
+            self.output = self.value + tf.subtract(self.advantage,
+                            tf.reduce_mean(self.advantage, axis=1, keepdims=True))
+            # Predicted Q value
+            self.Q = tf.reduce_sum(tf.multiply(self.output, self.actions), axis=1)
+            # Loss: mse between predicted and target Q values
+            self.loss = tf.reduce_mean(tf.square(self.target_Q - self.Q))
+            self.optimizer = RMSPropOptimizer(self.learningRate).minimize(self.loss)
         self.saver = tf.train.Saver()
