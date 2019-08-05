8bc0e827527dabeee73c376acc92f04465494ace
Jared Coughlin <jcoughl2@nd.edu>
2019-08-05 11:59:15 -0500

    Added losses.py. This is where all custom loss functions will live.

---
+++ losses.py
@@ -0,0 +1,30 @@
+"""
+Title:   losses.py
+Author:  Jared Coughlin
+Date:    8/5/19
+Purpose: Contains custom loss functions
+Notes:
+"""
+
+
+#============================================
+#                  per_mse
+#============================================
+def per_mse():
+    """
+    The Mean-Squared-Error loss function (MSE) as modified by
+    Prioritized-Experience-Replay (PER). See Schaul et al. 2016.
+
+    Parameters:
+    -----------
+        pass
+
+    Raises:
+    -------
+        pass
+
+    Returns:
+    --------
+        pass
+    """
+    pass
