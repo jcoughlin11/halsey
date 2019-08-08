Fixed a whole bunch of syntax errors. Also had forgotten to return absError from the fixed q and double dqn learn functions. Added a tf.reduce_sum() call to the calculation of absError since, when updating the priorities, each experience is assigned a float, not a sequence. It now runs!

agent.py
@@ -469,7 +469,7 @@ class Agent:
                 rewards,
                 nextStates,
                 dones,
-                isWeights
+                isWeights.flatten()
             )
         # Fixed-Q
         elif self.enableFixedQ:
@@ -479,7 +479,7 @@ class Agent:
                 rewards,
                 nextStates,
                 dones,
-                isWeights
+                isWeights.flatten()
             )
         # Standard dqn
         else:
@@ -489,7 +489,7 @@ class Agent:
                 rewards,
                 nextStates,
                 dones,
-                isWeights
+                isWeights.flatten()
             )
         # Update the priorities
         if self.enablePer:
@@ -562,8 +562,10 @@ class Agent:
         # Otherwise, the TD error for those terms would be equal to
         # the original Q value for that state-action entry
         qTarget[qTarget == 0] = qPred[qTarget == 0]
-        # Get the absolute value of the TD error for use in per
-        absError = tf.abs(qTarget - qPred)
+        # Get the absolute value of the TD error for use in per. The
+        # sum is so we only get 1 value per sample, since the priority
+        # for each experience is just a float, not a sequence
+        absError = tf.reduce_sum(tf.abs(qTarget - qPred), axis=1)
         # Update the network weights
         loss = self.qNet.model.train_on_batch(states, qTarget, sample_weight=isWeights)
         return loss, absError
@@ -625,10 +627,10 @@ class Agent:
             self.discountRate * qNext[nDoneInds, nextActions[nDoneInds]]
         qTarget[qTarget == 0] = qPred[qTarget == 0]
         # Get abs error
-        absError = tf.abs(qTarget - qPred)
+        absError = tf.reduce_sum(tf.abs(qTarget - qPred), axis=1)
         # Update the network weights
         loss = self.qNet.model.train_on_batch(states, qTarget, sample_weight=isWeights)
-        return loss
+        return loss, absError
 
     #-----
     # fixed_q_learn
@@ -676,7 +678,7 @@ class Agent:
             self.discountRate * np.amax(qNext[nDoneInds])
         qTarget[qTarget == 0] = qPred[qTarget == 0]
         # Get abs error
-        absError = tf.abs(qTarget - qPred)
+        absError = tf.reduce_sum(tf.abs(qTarget - qPred), axis=1)
         # Update the network weights
         loss = self.qNet.model.train_on_batch(states, qTarget, sample_weight=isWeights)
-        return loss
+        return loss, absError

nnio.py
@@ -97,7 +97,6 @@ def param_file_registers():
     type_register = {
         "floats": float_params,
         "ints": int_params,
-        'lists' : list_params,
         "strings": string_params,
     }
     return type_register
@@ -137,7 +136,7 @@ def check_agent_option_conflicts(params):
     if params['loss'] not in lossRegister:
         raise ValueError("Error, unrecognized loss function!")
     # Check for valid optimizer function
-    if params['loss'] not in optimizerRegister:
+    if params['optimizer'] not in optimizerRegister:
         raise ValueError("Error, unrecognized optimizer function!")
     # Double DQN requires fixed-Q
     if params["enable_double_dqn"] and not params["enable_fixed_Q"]:
@@ -363,13 +362,26 @@ def save_sumtree_memory(memBuffer, savePath):
         dg.create_dataset('rewards', (memBuffer.nLeafs,), dtype=np.float)
         dg.create_dataset('next_states', shape=stateShape, dtype=np.float)
         dg.create_dataset('dones', (memBuffer.nLeafs,), dtype=np.int)
-        # Loop over each sample in the buffer
+        # Loop over each sample in the buffer. If the buffer is not yet
+        # full then it will be comprised of both tuples and zeros
+        # This is not a particularly good way of doing this, I just
+        # wanted to get it running to test PER and I'll fix it in
+        # another branch dedicated to I/O. Instead of nLeafs tuples,
+        # it only goes up to np.where(memBuffer.data == 0)[0][0].
+        # Chaning this save affects the load, too, though
         for i, sample in enumerate(memBuffer.data):
-            dg['states'][i] = sample[0]
-            dg['actions'][i] = sample[1]
-            dg['rewards'][i] = sample[2]
-            dg['next_states'][i] = sample[3]
-            dg['dones'][i] = sample[4]
+            if isinstance(sample, tuple):
+                dg['states'][i] = sample[0]
+                dg['actions'][i] = sample[1]
+                dg['rewards'][i] = sample[2]
+                dg['next_states'][i] = sample[3]
+                dg['dones'][i] = sample[4]
+            else:
+                dg['states'][i] = np.zeros(memBuffer.data[0][0].shape)
+                dg['actions'][i] = sample
+                dg['rewards'][i] = sample
+                dg['next_states'][i] = np.zeros(memBuffer.data[0][0].shape)
+                dg['dones'][i] = sample
         
 
 