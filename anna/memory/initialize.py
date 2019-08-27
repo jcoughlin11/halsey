391ae14a3db8abebdbcee52d73ea299c82b40a86
Jared <jcoughl3@illinois.edu>
2019-08-27 16:02:41 -0500

    Added the memory module and initialize.py.

---
+++ anna/memory/initialize.py
@@ -0,0 +1,17 @@
+"""
+Title:   initialize.py
+Author:  Jared Coughlin
+Date:    8/27/19
+Purpose: Contains the functions related to setting up the agent's
+         memory object.
+Notes:
+"""
+
+
+#============================================
+#                init_memory
+#============================================
+def init_memory(params):
+    """
+    
+    """
