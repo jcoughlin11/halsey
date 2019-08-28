8d369eb25273c37f5ed83aa35ce58b5ccf09a495
Jared <jcoughl3@illinois.edu>
2019-08-28 13:54:33 -0500

    Added the loader and TrainParams classes.

---
+++ anna/training/utils.py
@@ -0,0 +1,17 @@
+"""
+Title:   utils.py
+Author:  Jared Coughlin
+Date:    8/28/19
+Purpose: Contains functions and classes related to making training
+         easier
+Notes:
+"""
+
+
+#============================================
+#                TrainParams
+#============================================
+class TrainParams:
+    """
+    
+    """
