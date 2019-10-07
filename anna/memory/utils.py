107172744a43763e9fa8f5df9f7a2ddc8f8268cc
Jared <jcoughl3@illinois.edu>
2019-10-07 12:04:55 -0500

    Added the navigator as an argument to get_new_memory.

--- anna/memory/utils.py
+++ anna/memory/utils.py
@@ -8,7 +8,7 @@ Notes:
 #============================================
 #              get_new_memory
 #============================================
-def get_new_memory(memoryParams, batchSize):
+def get_new_memory(memoryParams, batchSize, navigator):
     """
     Doc string.
 
@@ -24,8 +24,11 @@ def get_new_memory(memoryParams, batchSize):
     --------
         pass
     """
+    # Build the memory object of the desired type
     if memoryParams.mode == 'experience':
         memory = ExperienceMemory(memoryParams, batchSize)
     elif memoryParams.mode == 'episode':
         memory = EpisodeMemory(memoryParams, batchSize)
+    # Pre-populate the memory buffer to avoid the empty-memory problem
+    memory.pre_populate(navigator.env, navigator.frameHandler)
     return memory
