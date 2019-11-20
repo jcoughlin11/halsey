1f0e3085b0eff4e0854920fc3a34acc2168895af
Jared <jcoughl3@illinois.edu>
2019-11-19 18:35:46 -0600

    Added experience.py to anna/utils.

---
+++ anna/utils/experience.py
@@ -0,0 +1,37 @@
+"""
+Title: experience.py
+Purpose: Contains the Experience class.
+Notes:
+"""
+
+
+class Experience:
+    """
+    Doc string.
+
+    Attributes:
+    -----------
+        pass
+
+    Methods:
+    --------
+        pass
+    """
+
+    def __init__(self):
+        """
+        Doc string.
+
+        Parameters:
+        -----------
+            pass
+
+        Raises:
+        -------
+            pass
+
+        Returns:
+        --------
+            pass
+        """
+        pass
