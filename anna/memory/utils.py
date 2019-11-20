7a214f33e73b43346857e64edb3162825775efa1
Jared <jcoughl3@illinois.edu>
2019-11-19 18:35:16 -0600

    Removed the check for episodememory in memory/utils.py since that has not been implemented yet.

--- anna/memory/utils.py
+++ anna/memory/utils.py
@@ -3,7 +3,6 @@ Title: utils.py
 Purpose: Contains functions related to creating a new memory object.
 Notes:
 """
-from anna.memory.episode_memory import EpisodeMemory
 from anna.memory.experience_memory import ExperienceMemory
 
 
@@ -29,6 +28,4 @@ def get_new_memory(memoryParams):
     # Build the memory object of the desired type
     if memoryParams.mode == "experience":
         memory = ExperienceMemory(memoryParams)
-    elif memoryParams.mode == "episode":
-        memory = EpisodeMemory(memoryParams)
     return memory
