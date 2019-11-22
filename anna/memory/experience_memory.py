a282fa6b830cab1359731f03710a9f3035846f87
Jared <jcoughl3@illinois.edu>
2019-11-22 17:50:13 -0600

    Added parsing of the experiences to sample() in experience_memory.py.

--- anna/memory/experience_memory.py
+++ anna/memory/experience_memory.py
@@ -111,29 +111,44 @@ class ExperienceMemory:
 
         Parameters:
         -----------
-            batch_size : int
-                The size of the sample to be returned.
+            pass
+
         Raises:
         -------
             pass
+
         Returns:
         --------
-            sample : list
-                A list of randomly chosen experience tuples from the
-                buffer. Chosen without replacement. The length of the
-                list is batch_size.
+            pass
         """
         # Choose random indices from the buffer. Make sure the
-        # batch_size isn't larger than the current buffer size or np
-        # will complain
+        # batch_size isn't larger than the current buffer size
         try:
             indices = np.random.choice(
                 np.arange(len(self.buffer)), size=batchSize, replace=False
             )
         except ValueError:
             raise (
-                "Error, need batch_size < buf_size when sampling from memory!"
+                "Error, need batch size < buf size when sampling from memory!"
             )
-        # return STATES, NEXTSTATES, DONES, etc. processed. See the learn()
-        # return [self.buffer[i] for i in indices]
-        assert False
+        # Select randomly chosen sample
+        samples = np.array(self.buffer)[indices]
+        # Set up arrays for holding parsed info. tf expects the batch
+        # size to be the first argument of the shape
+        states = np.empty(
+            shape=[batchSize] + list(self.buffer[0].state.shape), dtype=np.float
+        )
+        actions = np.empty(shape=[batchSize, 1], dtype=np.int)
+        rewards = np.empty(shape=[batchSize, 1], dtype=np.float)
+        nextStates = np.empty(
+            shape=[batchSize] + list(self.buffer[0].state.shape), dtype=np.float
+        )
+        dones = np.empty(shape=[batchSize, 1], dtype=np.bool)
+        # Parse the sample
+        for i, sample in enumerate(samples):
+            states[i] = sample.state
+            actions[i] = sample.action
+            rewards[i] = sample.reward
+            nextStates[i] = sample.nextState
+            dones[i] = sample.done
+        return states, actions, rewards, nextStates, dones
