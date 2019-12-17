6789f3baf61b10e5ab4b394f58e2e4364b666d38
Jared <jcoughl3@illinois.edu>
2019-12-17 17:23:41 -0600

    Added env.py to the utils module.

---
+++ anna/utils/env.py
@@ -0,0 +1,29 @@
+"""
+Title: env.py
+Purpose:
+Notes:
+    * The environment handles interaction between the game and the
+        Agent
+"""
+
+
+# ============================================
+#                 build_env
+# ============================================
+def build_env():
+    """
+    Doc string.
+
+    Parameters:
+    -----------
+        pass
+
+    Raises:
+    -------
+        pass
+
+    Returns:
+    --------
+        pass
+    """
+    pass
