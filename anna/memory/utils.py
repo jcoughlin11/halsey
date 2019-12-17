15f0b6e3d0745f1d3570bc4d9c0635ac0ba7a878
Jared <jcoughl3@illinois.edu>
2019-12-17 17:22:55 -0600

    Added the memory module.

---
+++ anna/memory/utils.py
@@ -0,0 +1,31 @@
+"""
+Title: utils.py
+Purpose:
+Notes:
+    * The memory class stores information (experience) about previously
+        visited states
+    * The memory class handles constructing samples of data to use in
+        learning
+"""
+
+
+# ============================================
+#              get_new_memory
+# ============================================
+def get_new_memory():
+    """
+    Doc string.
+
+    Parameters:
+    -----------
+        pass
+
+    Raises:
+    -------
+        pass
+
+    Returns:
+    --------
+        pass
+    """
+    pass
