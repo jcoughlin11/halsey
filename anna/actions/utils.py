e645ca73efb2e028cc9ad50c0a7c85d4ed0bb36b
Jared <jcoughl3@illinois.edu>
2019-12-17 17:21:36 -0600

    Added the actions module.

---
+++ anna/actions/utils.py
@@ -0,0 +1,28 @@
+"""
+Title: utils.py
+Purpose:
+Notes:
+    * The action manager handles action selection
+"""
+
+
+# ============================================
+#          get_new_action_manager
+# ============================================
+def get_new_action_manager():
+    """
+    Doc string.
+
+    Parameters:
+    -----------
+        pass
+
+    Raises:
+    -------
+        pass
+
+    Returns:
+    --------
+        pass
+    """
+    pass
