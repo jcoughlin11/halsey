1457f3e6db205ce77c2935b9147a3ba2a258504e
Jared <jcoughl3@illinois.edu>
2019-12-17 19:52:20 -0600

    Added args to trainers' utils.py, filled in the __init__ method for QTrainer, and filled in the train() method, as well.

--- anna/trainers/qtrainer.py
+++ anna/trainers/qtrainer.py
@@ -1,6 +1,6 @@
 """
 Title: qtrainer.py
-Purpose: 
+Purpose:
 Notes:
 """
 import anna
@@ -25,7 +25,7 @@ class QTrainer:
     # -----
     # constructor
     # -----
-    def __init__(self):
+    def __init__(self, trainParams, navigator, brain, memory):
         """
         Doc string.
 
@@ -41,7 +41,18 @@ class QTrainer:
         --------
             pass
         """
-        pass
+        self.nEpisodes = trainParams.nEpisodes
+        self.maxEpisodeSteps = trainParams.mexEpisodeSteps
+        self.batchSize = trainParams.batchSize
+        self.savePeriod = trainParams.savePeriod
+        self.navigator = navigator
+        self.brain = brain
+        self.memory = memory
+        self.episode = 0
+        self.startEpisode = 0
+        self.earlyStop = False
+        self.doneTraining = False
+        self.episodeStep = 0
 
     # -----
     # train
@@ -62,4 +73,32 @@ class QTrainer:
         --------
             pass
         """
-        pass
+        # Loop over the desired number of training episodes
+        for self.episode in range(self.startEpisode, self.nEpisodes):
+            # Loop over the max number of steps allowed per episode
+            for self.episodeStep in range(self.maxEpisodeSteps):
+                # Check for early stopping
+                self.earlyStop = anna.utils.endrun.check_early_stop()
+                if self.earlyStop:
+                    break
+                # Transition to next state
+                experience = self.navigator.transition(self.brain)
+                # Save the experience
+                self.memory.add(experience)
+                # Update network weights
+                self.brain.learn(self.memory, self.batchSize)
+                # Update the brain's parameters (e.g., target q-network)
+                self.brain.update()
+                # Update memory, if applicable (e.g., priority weights)
+                self.memory.update()
+                # Check for terminal state
+                if experience.done:
+                    break
+            # Break out of the training loop if needed
+            if self.earlyStop:
+                break
+            # See if we need to save a checkpoint
+            if self.episode % self.savePeriod == 0:
+                yield
+        # If we get here, we're done
+        self.doneTraining = True
--- anna/trainers/utils.py
+++ anna/trainers/utils.py
@@ -5,12 +5,13 @@ Notes:
     * The trainer class oversees the navigator, brain, and memory
     * The trainer class contains the main training loop
 """
+import anna
 
 
 # ============================================
 #              get_new_trainer
 # ============================================
-def get_new_trainer():
+def get_new_trainer(folio):
     """
     Doc string.
 
@@ -27,11 +28,20 @@ def get_new_trainer():
         pass
     """
     # Create a new navigator
-    navigator = anna.navigation.utils.get_new_navigator()
+    navigator = anna.navigation.utils.get_new_navigator(
+        folio.navigation, folio.action, folio.frame, folio.run.envName
+    )
     # Create a new brain
-    brain = anna.brains.utils.get_new_brain()
+    brain = anna.brains.utils.get_new_brain(
+        folio.brain,
+        navigator.env.action_space.n,
+        navigator.frameManager.inputShape,
+        navigator.frameManager.channelsFirst,
+    )
     # Create a new memory object
-    memory = anna.memory.utils.get_new_memory()
-    if trainParams.mode == "qtrainer":
-        trainer = QTrainer()
+    memory = anna.memory.utils.get_new_memory(folio.memory)
+    if folio.training.mode == "qtrainer":
+        trainer = anna.trainers.qtrainer.QTrainer(
+            folio.training, navigator, brain, memory
+        )
     return trainer
