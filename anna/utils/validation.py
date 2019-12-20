80d346837f1348e8dce458a6562e696dd1fb5c61
Jared <jcoughl3@illinois.edu>
2019-12-20 16:44:04 -0600

    Reverted validation to the bare minimum. Decided to hold off on implementing it for another branch.

--- anna/utils/validation.py
+++ anna/utils/validation.py
@@ -8,29 +8,7 @@ Notes:
 # ============================================
 #                 Registers
 # ============================================
-# Run parameters
-runRegister = {"envName": str, "train": bool, "test": bool, "timeLimit": int}
-
-# Io parameters
-ioRegister = {"outputDir": str, "fileBase": str}
-
-# Training parameters
-trainingRegister = {
-    "nEpisodes": int,
-    "maxEpisodeSteps": int,
-    "batchSize": int,
-    "savePeriod": int,
-}
-
-trainersRegister = [
-    "qtrainer",
-]
-
-trainingRegister["trainers"] = trainersRegister
-
 # Brain parameters
-brainRegister = {"discount": float, "learningRate": float}
-
 convRegister = [
     "conv1",
 ]
@@ -42,34 +20,6 @@ duelingRegister = [
 rnnRegister = [
     "rnn1",
 ]
-
-lossRegister = []
-
-# Memory parameters
-memoryRegister = {
-    "maxSize": int,
-    "pretrainLen": int,
-}
-
-# Action parameters
-actionRegister = []
-
-# Navigation parameters
-navigationRegister = []
-
-# Frame parameters
-frameRegister = []
-
-registers = {
-    "run": runRegister,
-    "io": ioRegister,
-    "training": trainingRegister,
-    "brain": brainRegister,
-    "memory": memoryRegister,
-    "action": actionRegister,
-    "navigation": navigationRegister,
-    "frame": frameRegister,
-}
 
 
 # ============================================
@@ -91,10 +41,4 @@ def validate_params(folio):
     --------
         pass
     """
-    for section, sectionParams in registers.items():
-        # Make sure every required section is present in the folio
-        if section not in folio.__dict__.keys():
-            raise
-        # Now check each section's parameters
-        for paramName, paramValue in sectionParams.items():
-            pass
+    pass
