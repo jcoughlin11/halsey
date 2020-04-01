53325be9320bc691619fc64a6bcceeb6b586d460
Jared <jcoughl3@illinois.edu>
2020-04-01 17:47:12 -0500

    Fixed __init__.py files.

--- halsey/__init__.py
+++ halsey/__init__.py
@@ -1,14 +1,9 @@
 from . import (
-    console,
     brains,
     instructors,
-    io,
-    losses,
     memory,
     navigation,
     networks,
-    optimizers,
     pipelines,
     policies,
-    utils,
 )
--- halsey/io/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import (
-    logging,
-    read,
-)
