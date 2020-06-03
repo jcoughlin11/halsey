526f3994e43455429bf1e98c2d41fe61b1179c8f
Jared <jcoughl3@illinois.edu>
2020-06-03 15:26:04 -0500

    Added choose to epsilongreedy.py

--- halsey/explorers/epsilongreedy.py
+++ halsey/explorers/epsilongreedy.py
@@ -2,6 +2,9 @@
 Title: epsilongreedy.py
 Notes:
 """
+import numpy as np
+
+from halsey.utils.endrun import endrun
 
 
 # ============================================
@@ -22,3 +25,23 @@ class EpsilonGreedyExplorer:
         self.epsDecayRate = params["epsDecayRate"]
         self.epsilonStart = params["epsilonStart"]
         self.epsilonStop = params["epsilonStop"]
+
+    # -----
+    # choose
+    # -----
+    def choose(self, brain, env, frameStack):
+        """
+        Doc string.
+        """
+        exploitProb = np.random.random()
+        exploreProb = self.epsilonStop + (
+            self.epsilonStart - self.epsilonStop
+        ) * np.exp(-self.epsDecayRate * self.decayStep)
+        if exploreProb < 0.0:
+            msg = f"Probability of exploring is negative: `{exploreProb}`"
+            endrun(msg)
+        elif exploreProb >= exploitProb:
+            action = env.action_space.sample()
+        else:
+            action = np.argmax(brain.predict(frameStack).numpy())
+        return action
