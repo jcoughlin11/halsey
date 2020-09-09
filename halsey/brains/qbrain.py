40b563b8e9c922b1bbab19dd795143149612f10f
Jared Coughlin <jcoughl3@illinois.edu>
2020-09-08 23:00:04 -0500

    Fixes indentation typo and renames lossFunction.

--- halsey/brains/qbrain.py
+++ halsey/brains/qbrain.py
@@ -34,40 +34,40 @@ class QBrain(BaseBrain):
         with tf.GradientTape() as tape:
             beliefs = self.get_beliefs(states, actions)
             targets = self.get_targets(nextStates, dones, rewards)
-            loss = self.nets[0].lossFunction(tf.stop_gradient(targets), beliefs)
+            loss = self.nets[0].loss(tf.stop_gradient(targets), beliefs)
         gradients = tape.gradient(loss, self.nets[0].trainable_variables)
         self.nets[0].optimizer.apply_gradients(
             zip(gradients, self.nets[0].trainable_variables)
         )
 
-        # -----
-        # get_beliefs
-        # -----
-        def get_beliefs(self, states, actions):
-            """
-            Gets the network's current guess for the Q-value of each
-            action in the given state.
-            """
-            qVals = self.nets[0](states, training=True)
-            nActions = qVals.shape[1]
-            oneHot = qVals * tf.one_hot(actions, nActions, dtype=tf.float32)
-            qChosen = tf.reduce_sum(oneHot, 1)
-            return qChosen
+    # -----
+    # get_beliefs
+    # -----
+    def get_beliefs(self, states, actions):
+        """
+        Gets the network's current guess for the Q-value of each
+        action in the given state.
+        """
+        qVals = self.nets[0](states, training=True)
+        nActions = qVals.shape[1]
+        oneHot = qVals * tf.one_hot(actions, nActions, dtype=tf.float32)
+        qChosen = tf.reduce_sum(oneHot, 1)
+        return qChosen
 
-        # -----
-        # get_targets
-        # -----
-        def get_targets(self, nextStates, dones, rewards):
-            """
-            Bootstraps the "true" Q-values to compare to the beliefs
-            in the loss function.
-            """
-            qNext = self.nets[0](nextStates, training=True)
-            qNextMax = tf.reduce_max(qNext, 1)
-            dones = tf.cast(dones, qNextMax.dtype)
-            maskedVals = (1.0 - dones) * qNextMax
-            targets = rewards + self.discountRate * maskedVals
-            return targets
+    # -----
+    # get_targets
+    # -----
+    def get_targets(self, nextStates, dones, rewards):
+        """
+        Bootstraps the "true" Q-values to compare to the beliefs
+        in the loss function.
+        """
+        qNext = self.nets[0](nextStates, training=True)
+        qNextMax = tf.reduce_max(qNext, 1)
+        dones = tf.cast(dones, qNextMax.dtype)
+        maskedVals = (1.0 - dones) * qNextMax
+        targets = rewards + self.discountRate * maskedVals
+        return targets
 
     # -----
     # predict
