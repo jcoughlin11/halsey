10fa18c750f0a81db50460fbe0de6c0b5d716bbb
Jared Coughlin <jcoughl3@illinois.edu>
2020-09-12 14:44:25 -0500

    Started pipeline tests.

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,20 @@
 import nox
 
+    
+poetryURL = "https://raw.githubusercontent.com"
+poetryURL += "/python-poetry/poetry/master/get-poetry.py"
+
 
 @nox.session
 def lint(session):
-    session.install("black", "flake8")
+    session.install("black", "isort", "flake8")
+    session.run("isort", "--check", "halsey/")
     session.run("black", "--check", "halsey/")
     session.run("flake8", "halsey/")
+
+
+@nox.session
+def test_pipelines(session):
+    session.run("curl", "-ssL", poetryURL, "|", "python")
+    session.run("poetry", "install")
+    session.run("pytest", "-s", "-v", "tests/test_pipelines/") 
